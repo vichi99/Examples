ddbe0592d4622a1ad272874f9f7f2b24a7a567e6
vichi99 <john.vicha@seznam.cz>
2018-10-29 21:59:40 +0100

    -

--- cube.py
+++ cube.py
@@ -1,28 +1,54 @@
 ##########################
-#This code was created for fun and practice my skill
+# This code was created for fun and practice my skill
 ##########################
 import random
+
+
 def name():
     #name = input("Hi, whats your name? ")
     text = "Hi , whats your name? "
-    name = format_input(text,str)
+    name = format_input(text, str)
     length = len(name)
-    print("Hello {0}! Your name have {1} symbol".format(name,length))
+    print("Hello {0}! Your name have {1} symbol".format(name, length))
+
+
+def table():
+    text = "Table of number"
+    header_num = "| {:{width}} | {:{width}} | {:{width}} | {:{width}} |".format(
+        "dec", "bin", "oct", "hex", width=7)
+    header_text = "+{0:-^39}+\n" + header_num + "\n{1:->41}"
+    table_text = "| {0:{width}d} | {0:#{width}b} | {0:#{width}o} | {0:#{width}x} |"
+    print(header_text.format(text, "-", width=28))
+    for number in range(16):
+        print(table_text.format(number, width=7))
+    print("+{:->39}+".format("-"))
+
 
 def number():
-    format_text = "The number {0} is in binary: {0:0>4b} and the last value is {1}"
+    format_text = """The number {0}:
+    binary: {0:b} or {0:0>4b} or {0:0>#4b}
+    hex: {0:x} or {0:0>4x} or {0:0>#4x}
+    oct: {0:o} or {0:0>4o} or {0:0>#4o}
+    last value: {1}
+    Big number: {2:,}
+    Aligned text {0:>30}
+    center {0:*^25}"""
+    big_number = 1123456
     text = "Enter the int number: "
-    number = format_input(text,int)
+    number = format_input(text, int)
     length = len(str(number))
     if length == 1:
-        print(format_text.format(number,int(number % 10)))
+        print(format_text.format(number, int(number % 10), big_number))
     if length == 2:
-        print((format_text + ", {2}").format(number,int(number % 10),int(number % 100 / 10 )))
+        print((format_text + ", {2}").format(number,
+                                             (int(number % 10), int(number % 100 / 10)), big_number))
     if length >= 3:
-        print((format_text + ", {2}, {3}").format(number,int(number % 10),int(number % 100 / 10 ),int(number % 1000 / 100)))
+        print((format_text + ", {2}").format(number, (int(number %
+                                                          10), int(number % 100 / 10), int(number % 1000 / 100)), big_number))
     else:
         pass
     return
+
 
 def cube():
     text = "Enter the number of cube (1-6) you want to see, 7 to random choice or  >= 8 to see all: "
@@ -43,7 +69,7 @@ def cube():
     c4 = ("Number 4" + cube.format("o", "o", " ", " ", " ", "o", "o"))
     c5 = ("Number 5" + cube.format("o", "o", " ", "o", " ", "o", "o"))
     c6 = ("Number 6" + cube.format("o", "o", "o", " ", "o", "o", "o"))
-    cube_list = [c1,c2,c3,c4,c5,c6]
+    cube_list = [c1, c2, c3, c4, c5, c6]
     wrong = ("Wrong enter")
     error = ("Error enter")
     try:
@@ -57,7 +83,8 @@ def cube():
         print(error)
     return
 
-def format_input(text,exp_type):
+
+def format_input(text, exp_type):
     cond = True
     while cond == True:
         value = input(text)
@@ -79,9 +106,10 @@ def format_input(text,exp_type):
             else:
                 print("The input type in code is wrong")
         except:
-                print("The input is wrong. Type {} number".format(str(exp_type)))
+            print("The input is wrong. Type {} number".format(str(exp_type)))
 
 
+table()
 name()
 number()
 cube()
