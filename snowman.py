0d33b71e5b5f882c7db1fd43bc48851083e9d583
vichi99 <john.vicha@seznam.cz>
2018-11-10 20:35:40 +0100

    -

--- snowman.py
+++ snowman.py
@@ -2,7 +2,11 @@ import tkinter
 name = "Snowman"
 width = 640
 height = 480
-r = 100
+
+r = 90
+width_button = r / 10
+width_eyes = width_button / 2
+width_mouth = width_button / 2.5
 x, y = width / 2 - r, height - 2 * r
 
 canvas = tkinter.Canvas(width=width, height=height)
@@ -19,16 +23,35 @@ def ball(x, y, r):
     canvas.create_oval(coordinates, width=2, outline='', fill="white")
 
 
+def button(x, y, r):
+    x = x + r
+    for i in [r / 2, r, r * 1.5]:
+        canvas.create_oval(x, y + i, x, y + i, width=width_button)
+
+
+def face(x, y, r):
+    x1 = x + r/2
+    y1 = y + r/2
+    x2 = x1 + r
+    for x, y in [[x1 , y1],[x2,y1],[x+r,y+r]]:
+        canvas.create_oval(x, y, x, y, width=width_mouth)
+    y2 = y + r/2
+    canvas.create_line(x1,y2,x2,y2,width=width_mouth)
+
+
 def snowman(x, y, r):
     ball(x, y, r)
+    button(x, y, r)
     x = x + r / 2 * 2 / 3
     y = y - r * 2 / 3 * 2
     r = r * 2 / 3
     ball(x, y, r)
+    button(x, y, r)
     x = x + r / 2 * 2 / 2
     y = y - r * 1 / 2 * 2
     r = r * 1 / 2
     ball(x, y, r)
+    face(x, y, r)
 
 
 # ball(x,y+height/4,r)
