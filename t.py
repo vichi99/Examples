35dee3b9deb653161a1e5ddf847599bd7f9b65a2
vichi99 <john.vicha@seznam.cz>
2018-11-19 14:49:48 +0100

    -

---
+++ t.py
@@ -0,0 +1,14 @@
+import turtle
+def square(length):
+    for i in range(4):
+        t.fd(length)
+        t.rt(90)
+
+t = turtle.Turtle()
+square(100)
+t.pu()
+t.lt(70)
+t.fd(80)
+t.pd()
+
+square(50)
