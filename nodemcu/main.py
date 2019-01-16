6301bc69b7e53e5b624c660a3e4c094a43874910
vichi99 <john.vicha@seznam.cz>
2019-01-16 22:29:14 +0100

    -

---
+++ nodemcu/main.py
@@ -0,0 +1,13 @@
+
+class Test:
+    def __init__(self,name):
+        self.name = name
+
+    def __str__(self):
+        card = "===========================\n"
+        return card
+jan = Test("John")
+# print("Surname: {}" .format(porter.surname))
+# print("Full name: {}".format(porter.full_name))
+# print("Birthday: {} ; Age: {}".format(porter.birthday, porter.age()))
+# print(porter.card())
