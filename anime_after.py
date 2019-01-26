6ffca379b98de46f67fe99ca4d728204578f5fdd
vichi99 <john.vicha@seznam.cz>
2019-01-27 00:03:13 +0100

    -

--- anime_after.py
+++ anime_after.py
@@ -1,7 +1,7 @@
 ##############################################################################
 # The program do not nothing special, only shows function after via animation
 ##############################################################################
-import tkinter, random, mathS
+import tkinter, random, math
 canvas = tkinter.Canvas(width=640, height=480)
 canvas.pack()
 
